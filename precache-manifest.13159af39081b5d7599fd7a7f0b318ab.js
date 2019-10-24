6105e525bb5410485b34d41b11d0bea72e02914b
Mar Sánchez <mmiranda@nearsoft.com>
2019-10-24 11:06:56 -0500

    Updates

---
+++ precache-manifest.13159af39081b5d7599fd7a7f0b318ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cf070df2bb3faf8e15587daf28d073a",
+    "url": "/todo-redux/index.html"
+  },
+  {
+    "revision": "8887aad202808f1652c4",
+    "url": "/todo-redux/static/css/main.437fb5af.chunk.css"
+  },
+  {
+    "revision": "558e18a1bdd4c0792e57",
+    "url": "/todo-redux/static/js/2.16050599.chunk.js"
+  },
+  {
+    "revision": "8887aad202808f1652c4",
+    "url": "/todo-redux/static/js/main.b854e12e.chunk.js"
+  },
+  {
+    "revision": "dc9f27f54dbeb5fb77f5",
+    "url": "/todo-redux/static/js/runtime-main.c3caf08c.js"
+  }
+]);
